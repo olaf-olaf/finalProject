a1ddf708ba331975af1d0e41700e4ed7271df72f
Olaf Kroon <Olaf@macbook-pro-van-olaf-2.local>
2017-01-20 16:28:26 +0100

    improved the visual aspect of the app

--- drumPad/ViewController.swift
+++ drumPad/ViewController.swift
@@ -11,7 +11,9 @@ import AudioKit
 
 
 class ViewController: UIViewController {
-    
+   
+    @IBOutlet weak var mixerButton: UIButton!
+    @IBOutlet weak var fxButton: UIButton!
     @IBOutlet weak var metronomeTempoSlider: UISlider!
     @IBOutlet weak var metronomeButton: UIButton!
     @IBOutlet weak var nextKit: UIButton!
@@ -22,10 +24,24 @@ class ViewController: UIViewController {
     @IBOutlet weak var hiHatPad: UIButton!
     @IBOutlet weak var snarePad: UIButton!
     
+    let enabledColor = UIColor(red: (246/255.0), green: (124/255.0), blue: (113/255.0), alpha: 1.0)
+    let disabledColor = UIColor(red: (245/255.0), green: (245/255.0), blue: (245/255.0), alpha: 1.0)
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         kitDisplay.text = AudioController.sharedInstance.LEDKitSelector.displayKit
         metronomeTempoSlider.value = Float(AudioController.sharedInstance.currentFrequency)
+        
+        nextKit.layer.cornerRadius = 5
+        previousKit.layer.cornerRadius = 5
+        metronomeButton.layer.cornerRadius = 5
+        mixerButton.layer.cornerRadius = 5
+        fxButton.layer.cornerRadius = 5
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -33,25 +49,54 @@ class ViewController: UIViewController {
                // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func touchDownMixer(_ sender: UIButton) {
+        mixerButton.backgroundColor = enabledColor
+    }
+    
+    @IBAction func downFxbutton(_ sender: UIButton) {
+        fxButton.backgroundColor = enabledColor
+    }
+    
+    
+    @IBAction func touchDownNext(_ sender: UIButton) {
+        
+        nextKit.backgroundColor = enabledColor
+    }
     @IBAction func setNextDisplay(_ sender: Any) {
         if nextKit.isTouchInside {
             AudioController.sharedInstance.LEDKitSelector.displayNext()
             kitDisplay.text =  AudioController.sharedInstance.LEDKitSelector.displayKit
             AudioController.sharedInstance.replaceKit(kitName: kitDisplay.text!)
+            nextKit.backgroundColor = disabledColor
         }
+        
     }
+    
+    
+    @IBAction func touchDownPrevious(_ sender: UIButton) {
+        previousKit.backgroundColor = enabledColor
+    }
+    
     
     @IBAction func setPreviousDisplay(_ sender: Any) {
         if previousKit.isTouchInside {
             AudioController.sharedInstance.LEDKitSelector.displayPrevious()
             kitDisplay.text = AudioController.sharedInstance.LEDKitSelector.displayKit
             AudioController.sharedInstance.replaceKit(kitName: kitDisplay.text!)
+            previousKit.backgroundColor = disabledColor
+
         }
     }
     
     @IBAction func enableMetronome(_ sender: Any) {
         if metronomeButton.isTouchInside {
             AudioController.sharedInstance.setMetronome()
+            if AudioController.sharedInstance.generator.isPlaying {
+                metronomeButton.backgroundColor = enabledColor
+            } else {
+                 metronomeButton.backgroundColor = disabledColor
+                
+            }
         }
     }
     
