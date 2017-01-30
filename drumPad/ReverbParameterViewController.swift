c0a485ec90e5f7663acd78a39e84cd4fcdd6df1b
Olaf Kroon <Olaf@macbook-pro-van-olaf-2.local>
2017-01-30 17:14:43 +0100

    commit to pull

--- drumPad/ReverbParameterViewController.swift
+++ drumPad/ReverbParameterViewController.swift
@@ -24,7 +24,7 @@ class ReverbParameterViewController: UIViewController {
         super.viewDidLoad()
         setReverbButton.layer.cornerRadius = 5
         
-        // Voor dit initialisen misschien een method maken
+        // Voor dit initialisen misschien een method maken.
         decayKnob = Knob(frame: decayKnobPlaceHolder.bounds)
         decayKnobPlaceHolder.addSubview(decayKnob)
         decayKnob.lineWidth = 5.0
