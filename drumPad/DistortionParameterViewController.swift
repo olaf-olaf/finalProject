89af60c81027cef3a3b8b67360c378e86f0d4127
Olaf Kroon <Olaf@wcw-stud-145-109-3-166.wireless.uva.nl>
2017-01-31 10:40:44 +0100

    updated some knobs

--- drumPad/DistortionParameterViewController.swift
+++ drumPad/DistortionParameterViewController.swift
@@ -13,8 +13,8 @@ import UIKit
 class distortionParametersViewController: UIViewController {
     let enabledColor = UIColor(red: (246/255.0), green: (124/255.0), blue: (113/255.0), alpha: 1.0)
     
-    var DecimationKnob: Knob!
-    var RoundingKnob: Knob!
+    var decimationKnob: Knob!
+    var roundingKnob: Knob!
     
     @IBOutlet weak var setDistortion: UIButton!
     @IBOutlet weak var roundingKnobPlaceholder: UIView!
@@ -24,21 +24,15 @@ class distortionParametersViewController: UIViewController {
         super.viewDidLoad()
         setDistortion.layer.cornerRadius = 5
         
-        DecimationKnob = Knob(frame: decimationKnobPlaceholder.bounds)
-        decimationKnobPlaceholder.addSubview(DecimationKnob)
-        DecimationKnob.lineWidth = 5.0
-        DecimationKnob.pointerLength = 10.0
-        DecimationKnob.minimumValue = 0
-        DecimationKnob.maximumValue = 1
-        DecimationKnob.value = Float(AudioController.sharedInstance.distortion.decimation)
+        decimationKnob = Knob(frame: decimationKnobPlaceholder.bounds)
+        decimationKnobPlaceholder.addSubview(decimationKnob)
+        decimationKnob.setKnobDisplay(largeButton: true, minimum: 0, maximum: 1)
+        decimationKnob.value = Float(AudioController.sharedInstance.distortion.decimation)
         
-        RoundingKnob = Knob(frame: roundingKnobPlaceholder.bounds)
-        roundingKnobPlaceholder.addSubview(RoundingKnob)
-        RoundingKnob.lineWidth = 5.0
-        RoundingKnob.pointerLength = 10.0
-        RoundingKnob.minimumValue = 0
-        RoundingKnob.maximumValue = 1
-        RoundingKnob.value = Float(AudioController.sharedInstance.distortion.rounding)
+        roundingKnob = Knob(frame: roundingKnobPlaceholder.bounds)
+        roundingKnobPlaceholder.addSubview(roundingKnob)
+        roundingKnob.setKnobDisplay(largeButton: true, minimum: 0, maximum: 1)
+        roundingKnob.value = Float(AudioController.sharedInstance.distortion.rounding)
     }
 
     override func didReceiveMemoryWarning() {
@@ -47,8 +41,8 @@ class distortionParametersViewController: UIViewController {
     
     @IBAction func setDistortionParameters(_ sender: UIButton) {
         setDistortion.backgroundColor = enabledColor
-        let decimation = Double(DecimationKnob.value)
-        let rounding = Double(RoundingKnob.value)
+        let decimation = Double(decimationKnob.value)
+        let rounding = Double(roundingKnob.value)
         AudioController.sharedInstance.setDistortionParameters(distortionDecimation: decimation, distortionRouding: rounding)
     }
 }
